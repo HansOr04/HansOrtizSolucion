5e25d48e14f98aab391e205b37595256dbee56e6
Hans Ortiz <127800719+HansOr04@users.noreply.github.com>
2024-12-01 14:32:24 -0500

    app

---
+++ APPConsultasHansOrtiz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APPConsultasHansOrtiz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APPConsultasHansOrtiz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace APPConsultasHansOrtiz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
